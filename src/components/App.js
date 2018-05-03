d2ffd503f8fb6c6c6345f69c5a785e3266ec8600
mvaden <mvaden08@gmail.com>
2018-05-03 01:48:05 -0400

    Button + Modal, showing a dialog.

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,12 @@ import React from 'react'
 import Footer from './Footer'
 import AddTodo from '../containers/AddTodo'
 import VisibleTodoList from '../containers/VisibleTodoList'
+import Dialog from './Dialog'
 
 const App = () => (
   <div>
     <AddTodo />
+    <Dialog />
     <VisibleTodoList />
     <Footer />
   </div>
---
+++ src/components/Dialog.js
@@ -0,0 +1,39 @@
+import React from "react";
+import Modal from "react-responsive-modal";
+
+const styles = {
+    fontFamily: "sans-serif",
+    textAlign: "center"
+};
+
+export default class Dialog extends React.Component {
+
+    state = {
+        open: false
+    };
+
+    onOpenModal = () => {
+        this.setState({
+            open: true
+        });
+    };
+
+    onCloseModal = () => {
+        this.setState({
+            open: false
+        });
+    };
+
+    render() {
+        const { open } = this.state;
+        return ( 
+            <div style={styles}>
+                <button onClick={this.onOpenModal}>Open modal</button> 
+                <Modal open={open} onClose={this.onCloseModal} little>
+                    <h2>How are you feeling today?</h2> 
+                    <p>Below is an area for leaving feedback. From the select dropdown and input, please leave your name and a review. Thank you!</p>
+                </Modal> 
+            </div>
+        );
+    }
+}
